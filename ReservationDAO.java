f314918e555851e1ef41430dec0e45be3665ad4a
Omar Abdelrahman <95719214+OmarA0606@users.noreply.github.com>
2025-04-19 15:28:38 +0200

    Create ReservationDAO.java

---
+++ ReservationDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import Modele.Reservation;
+import java.util.List;
+
+public interface ReservationDAO {
+    void create(Reservation reservation);
+    Reservation findById(int ID_reservation);
+    List<Reservation> findAll();
+    void update(Reservation reservation);
+    void delete(int ID_reservation);
+    List<Reservation> findByUserId(int userID);
+}
