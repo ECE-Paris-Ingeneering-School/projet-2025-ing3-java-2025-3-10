88d3149e4ff4b1ad80a3d4518f3db592934c0e8f
romligsky <romainligneres77@gmail.com>
2025-04-19 13:36:43 +0200

    Update InscriptionControleur.java

--- InscriptionControleur.java
+++ InscriptionControleur.java
@@ -3,7 +3,12 @@ package Controleur;
 import DAO.UsersDAO;
 import Modele.Users;
 import Vue.InscriptionVue;
-import javax.swing.JOptionPane;
+import Vue.ListeLogementsVue;
+import Vue.ConnexionVue;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class InscriptionControleur {
 
@@ -17,51 +22,53 @@ public class InscriptionControleur {
     }
 
     private void initController() {
+        // Ecoute du bouton "S'inscrire"
         inscriptionVue.getBtnInscription().addActionListener(e -> creerCompte());
+
+        // Ecoute du label "J'ai déjà un compte" (clic)
+        inscriptionVue.getLabelDejaCompte().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ouvrirConnexion();
+            }
+        });
     }
 
     private void creerCompte() {
-        // Récupérer les champs de la vue
-        String email = inscriptionVue.getEmail();
-        String mdp   = inscriptionVue.getMotDePasse();
-        String nom   = inscriptionVue.getNom();
-        String prenom= inscriptionVue.getPrenom();
+        String email  = inscriptionVue.getEmail();
+        String mdp    = inscriptionVue.getMotDePasse();
+        String nom    = inscriptionVue.getNom();
+        String prenom = inscriptionVue.getPrenom();
 
-        // Vérifier si l'email n'existe pas déjà
         if (usersDAO.findByEmail(email) != null) {
             inscriptionVue.afficherMessage("Cette adresse email est déjà utilisée !");
             return;
         }
 
-        // Par défaut, on suppose qu'un nouvel inscrit est un client (role=0).
-        // Si vous gérez un champ "role" différent, adaptez.
-        Users newUser = new Users(
-                0,            // ID (si vous avez un auto-increment, vous pouvez mettre un placeholder)
-                0,            // role=0 => client, 1 => admin
-                email,
-                mdp,
-                nom,
-                prenom
-        );
+        // role=0 => client
+        Users newUser = new Users(0, 0, email, mdp, nom, prenom);
         usersDAO.create(newUser);
 
-        // Après la création, on peut récupérer l'ID généré si auto-increment,
-        // par ex. newUser.setID(<ID retourné>); selon votre DAO.
-
         inscriptionVue.afficherMessage("Compte créé avec succès !\nConnexion automatique...");
-
-        // Fermer la fenêtre d'inscription
         inscriptionVue.dispose();
 
-        // Connexion automatique : rôle=0 => client, rôle=1 => admin, etc.
-        if (newUser.getRole() == 1) {
-            // Admin
-            AdminControleur adminCtrl = new AdminControleur(newUser);
-            adminCtrl.afficherMenuAdmin();
-        } else {
-            // Client
-            ClientControleur clientCtrl = new ClientControleur(newUser);
-            clientCtrl.afficherMenuClient();
-        }
+        // Connexion auto et affichage des logements
+        ListeLogementsVue accueilVue = new ListeLogementsVue(newUser);
+        accueilVue.setVisible(true);
+
+        // Charger tous les logements après la connexion
+        LogementControleur logementControleur = new LogementControleur(newUser);
+        logementControleur.afficherListeLogements();  // Charger et afficher tous les logements pour le client
+    }
+
+    private void ouvrirConnexion() {
+        // Fermer l'inscription
+        inscriptionVue.dispose();
+
+        // Ouvrir la connexion
+        ConnexionVue connexionVue = new ConnexionVue();
+        new ConnexionControleur(connexionVue, this.usersDAO);
+        connexionVue.setVisible(true);
     }
 }
+
