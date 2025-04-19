391d2256245645c4250a196b75d2e3e40307ba06
sachagirszyn <130834768+sachagirszyn@users.noreply.github.com>
2025-04-19 15:41:43 +0200

    Create StatsVue.java

---
+++ StatsVue.java
@@ -0,0 +1,14 @@
+package Vue;
+
+import javax.swing.*;
+
+public class StatsVue extends JFrame {
+
+
+    public StatsVue() {
+        setTitle("Statistiques - Booking");
+        setSize(1200, 1000);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    }
