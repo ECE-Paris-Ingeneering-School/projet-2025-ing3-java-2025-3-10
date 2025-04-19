65a4fdb2588d1e32c3d9f3b141caefe0816e76a3
Guslpk <augustin.lipka59@gmail.com>
2025-04-19 15:18:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
+import Controleur.ConnexionControleur;
+import DAO.UsersDAO;
+import DAO.UsersDAOImpl;
+import Vue.ConnexionVue;
 
-import Controleur.AccueilControleur;      // Contrôleur de la page d'accueil
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Charger le driver MySQL (optionnel si vous le faites ailleurs)
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -13,15 +17,23 @@ public class Main {
             System.exit(1);
         }
 
-
+        // Optionnel : configurer le look & feel natif
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
-            // pas bloquant si échec
             e.printStackTrace();
         }
 
-        AccueilControleur accueilControleur = new AccueilControleur();
-        accueilControleur.afficherAccueil();
+        // Créer la vue de connexion
+        ConnexionVue connexionVue = new ConnexionVue();
+
+        // Créer le DAO (pour la table Users)
+        UsersDAO usersDAO = new UsersDAOImpl();
+
+        // Créer le contrôleur de connexion en passant la vue + le DAO
+        new ConnexionControleur(connexionVue, usersDAO);
+
+        // Afficher la fenêtre de connexion
+        connexionVue.setVisible(true);
     }
 }
